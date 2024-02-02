d64aff61068a44ce53512b235493a5db2a08dc1a
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-02-02 13:19:09 +0000

    Update cb_short_hero.php

--- page-templates/blocks/cb_short_hero.php
+++ page-templates/blocks/cb_short_hero.php
@@ -1,8 +1,10 @@
 <?php
-$img = wp_get_attachment_image_url(get_field('background'), 'full');// ?? null;
+$f = get_the_post_thumbnail_url(get_the_ID(), 'full');
+$img = wp_get_attachment_image_url(get_field('background'), 'full') ?: $f;
 $class = $block['className'] ?? null;
 ?>
-<section class="short-hero d-flex <?=$class?>" style="background-image:url(<?=$img?>)">
+<section class="short-hero d-flex <?=$class?>"
+    style="background-image:url(<?=$img?>)">
     <div class="overlay--light"></div>
     <div class="container-xl d-flex flex-column justify-content-center">
         <div class="row">
@@ -13,10 +15,12 @@ $class = $block['className'] ?? null;
                 <?php
                 if (get_field('content')) {
                     ?>
-                <div class="text-white" data-aos="fade"><?=get_field('content')?></div>
-                    <?php
+                <div class="text-white" data-aos="fade">
+                    <?=get_field('content')?>
+                </div>
+                <?php
                 }
-                ?>
+?>
             </div>
         </div>
     </div>
